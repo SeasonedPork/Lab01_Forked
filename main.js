28fdf2a9c4eb05ffb511c518b36ab9f6abea85cb
Slurp Slurp <yanawarood.yk@gmail.com>
2022-07-01 09:28:03 +0700

    5.4

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             inStock: true,
             inventory: 100,
+            details: ['50% cotton','30% wool','20% polyster'],
+            variants: [
+                { id: 2234, color: 'green' , size:'  S  ' },
+                { id: 2235, color: 'blue', size:'  M  '},
+                { id: 69, color: 'null', size:' L '}
+            ],
+            cart: 0,
             onSale: false
         }
     }
